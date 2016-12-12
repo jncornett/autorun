6e52b41a2458d055d130f4428d8f85fa348e80aa
Joel Cornett <joel.cornett@gmail.com>
2016-12-12 12:22:37 -0500

    updated files

--- autorun/__main__.py
+++ autorun/__main__.py
@@ -5,30 +5,81 @@ import logging
 import fnmatch
 import time
 import subprocess
+import sys
+import shlex
 
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 
 
-class MyHandler(FileSystemEventHandler):
+class Map(dict):
+    def __missing__(self, key):
+        return ''
+
+
+def format_ansi(mappings, string):
+    return string.format_map(Map(mappings))
+
+
+def isatty():
+    return sys.stderr.isatty()
+
+
+class PlainFormatter(logging.Formatter):
+    STYLE = {}
+    def format(self, record):
+        msg = format_ansi(self.STYLE, record.msg)
+        level = record.levelname.lower()
+        level_fmt = '{%s}%s{/}' % (level, level)
+        level_out = format_ansi(self.STYLE, level_fmt)
+        return '[ %s ]: %s' % (level_out, msg % record.args)
+
+
+class TTYFormatter(PlainFormatter):
+    STYLE = {
+        'debug': '\x1b[34m',
+        'info': '\x1b[32m',
+        'warning': '\x1b[33m',
+        'error': '\x1b[31m',
+        'path': '\x1b[35m',
+        'cmd': '\x1b[36m',
+        '/': '\x1b[0m'
+    }
+
+
+class EventHandler(FileSystemEventHandler):
     def __init__(self, filter, command):
         self.filter = filter
         self.command = command
 
     def on_any_event(self, e):
         if self.filter(e):
+            logging.getLogger(__name__).info(
+                '{path}%s{/} %s', e.src_path, e.event_type)
             self.command(e)
         else:
-            logging.debug("ignoring %s on %r", e.event_type, e.src_path)
+            logging.getLogger(__name__).debug(
+                '{path}%s{/} %s ignored', e.src_path, e.event_type)
 
 
 def parse_args():
     p = argparse.ArgumentParser()
-    p.add_argument("path")
-    p.add_argument("command", nargs=argparse.REMAINDER)
-    p.add_argument("-i", "--include", action='append')
-    p.add_argument("-x", "--exclude", action='append')
-    p.add_argument("--debug", action='store_true')
+    p.add_argument('path', help='the path to watch (recursive)')
+    p.add_argument('command',
+        nargs=argparse.REMAINDER,
+        help='remaining positional arguments as the command to execute')
+    p.add_argument('-i', '--include',
+        action='append', metavar='PATTERN',
+        help='glob pattern to include. can be specified more than once')
+    p.add_argument('-x', '--exclude',
+        action='append', metavar='PATTERN',
+        help='glob pattern to exclude. can be specified more than once')
+    p.add_argument('-q', '--quiet',
+        action='store_true',
+        help='set verbosity to a minimum')
+    p.add_argument('--debug',
+        action='store_true',
+        help='set verbosity very high')
     opts = p.parse_args()
     return opts
 
@@ -60,22 +111,42 @@ def get_filter(includes, excludes):
 
     return inner
 
+
 def get_command(args):
     def inner(e):
-        logging.info("%r changed", e.src_path)
-        cmd = [x.format(e.src_path) for x in args]
+        cmd = [x.replace('%', e.src_path) for x in args]
+        logging.getLogger(__name__).info(
+            'running {cmd}%s{/}', ' '.join(map(shlex.quote, cmd)))
         try:
             subprocess.check_call(cmd)
-        except subprocess.CalledProcessError as e:
-            logging.error("%s: %s", cmd, e)
+        except subprocess.CalledProcessError as error:
+            logging.getLogger(__name__).error(
+                'returned %d', error.returncode)
 
     return inner
 
 
+def setup_logging(logger, tty, debug, quiet):
+    if debug:
+        logger.setLevel(logging.DEBUG)
+    elif quiet:
+        logger.setLevel(logging.ERROR)
+    else:
+        logger.setLevel(logging.INFO)
+
+    handler = logging.StreamHandler()
+    if tty:
+        handler.setFormatter(TTYFormatter())
+    else:
+        handler.setFormatter(PlainFormatter())
+
+    logger.addHandler(handler)
+
+
 def main():
     opts = parse_args()
-    logging.basicConfig(level=logging.DEBUG if opts.debug else logging.INFO)
-    handler = MyHandler(
+    setup_logging(logging.getLogger(__name__), isatty(), opts.debug, opts.quiet)
+    handler = EventHandler(
         get_filter(opts.include, opts.exclude),
         get_command(opts.command)
     )
@@ -84,7 +155,7 @@ def main():
     observer.start()
     try:
         while True:
-            time.sleep(0.5)
+            time.sleep(1)
     except KeyboardInterrupt:
         observer.stop()
     observer.join()
